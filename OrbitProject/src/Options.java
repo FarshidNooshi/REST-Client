adcb8a6b13fc13f7e51da23d44cc9aab79cdc472
FarshidNooshi@users.noreply.github.com <FarshidNooshi@users.noreply.github.com>
2020-05-12 22:56:01 +0430

    easy easy tamam tamam :) !

--- OrbitProject/src/Options.java
+++ OrbitProject/src/Options.java
@@ -2,8 +2,6 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Options extends JFrame {
     private boolean isDarkMode, isExitable;
