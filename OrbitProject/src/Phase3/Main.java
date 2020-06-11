81d70758ee96891ba29f4ed1b93fd190108f6ccc
Farshid726 <Farshidnooshi726@aut.ac.ir>
2020-06-11 23:50:44 +0430

    learned something cool to add for tomorrow.

--- OrbitProject/src/Phase3/Main.java
+++ OrbitProject/src/Phase3/Main.java
@@ -2,6 +2,7 @@ package Phase3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("going to get this thing done. :)");
+        Phase1.Main.main(args);
+
     }
 }
